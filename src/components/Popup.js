445a2e6428296c6796e9dda5c90a9adf8b5ef8cc
Bernardo Bustamante-Martinez <bbustam3@asu.edu>
2020-09-10 16:18:54 -0700

    Fixed build for GH pages

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,6 +5,7 @@ function Popup({ selected, closePopup }) {
 		<section className='popup'>
 			<div className='content'>
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
+				<p className='rating'>Rated: {selected.Rated}</p>
 				<p className='rating'>Rating: {selected.imdbRating}</p>
 				<div className='plot'>
 					<img src={selected.Poster} />
