eedd661de9b0429496b315bbd844bc2f085252b6
Bernardo Bustamante-Martinez <bbustam3@asu.edu>
2020-09-10 16:24:07 -0700

    Updates

---
+++ precache-manifest.094e3f979bbdb6107cb84729686c5636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c37d0761c762b5c81b29ff2f3c5be7",
+    "url": "/shopifymoviedb/index.html"
+  },
+  {
+    "revision": "7b3e9ad07b15478eb05e",
+    "url": "/shopifymoviedb/static/css/main.0d0ac7ab.chunk.css"
+  },
+  {
+    "revision": "b37917ff2ceddccdee39",
+    "url": "/shopifymoviedb/static/js/2.a7a6cab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopifymoviedb/static/js/2.a7a6cab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3e9ad07b15478eb05e",
+    "url": "/shopifymoviedb/static/js/main.6180bb0b.chunk.js"
+  },
+  {
+    "revision": "79784e2c7b695bf565c6",
+    "url": "/shopifymoviedb/static/js/runtime-main.815d367c.js"
+  }
+]);
